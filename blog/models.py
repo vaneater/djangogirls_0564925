be7b38f22a280d8732fd6bda7558844a89175869
vanessa <vanessagatner@gmail.com>
2020-06-11 15:59:18 +0200

    Added view and template for detailed blog post as well as CSS for the site. weil ich die neuste version kaputt gemacht habe nochmal. ups

--- blog/models.py
+++ blog/models.py
@@ -4,15 +4,9 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-
-    #TOPICS = {'R', 'Random'
-    #           'F', 'Food'
-    #           'R', 'Tech'}
-
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    #topic = models.CharField('Random', 'Food', 'Tech')
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
